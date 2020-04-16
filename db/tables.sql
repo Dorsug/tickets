6bcc7e1d26b1026d0b440bb869c02089dfb86ea0
Hugo Dorfsman <hugo@woup.net>
2020-04-16 10:43:23 +0200

    Réécrit des tables pour sqlite sql:table::Structure

--- db/tables.sql
+++ db/tables.sql
@@ -1,14 +1,15 @@
 -- Liste les ateliers disponible
 CREATE TABLE Atelier (
   id INTEGER PRIMARY KEY,    -- Clé primaire atelier
-  -- structure INT NOT NULL,    -- Clé secondaire de la structure
+  structure INT NOT NULL,    -- Clé secondaire de la structure
   numero INT NOT NULL,       -- Numéro d’atelier
   nom VARCHAR(100) NOT NULL, -- Nom de l’atelier
   description TEXT,          -- Description de l’atelier
   age_mini INT NOT NULL,      -- Age minimum pour participer à l'atelier
   age_maxi INT NOT NULL,      -- Age maximum pour participer à l'atelier
   nombreplace INT NOT NULL,  -- Nombre de participants prévu'
-  prix DECIMAL(6,2) NOT NULL -- Coût de l'aterlier
+  prix DECIMAL(6,2) NOT NULL, -- Coût de l'aterlier
+  CONSTRAINT Atelier_fk_1 FOREIGN KEY (structure) REFERENCES Structure (id)
 );
 
 -- Séance dispensées par les ateliers
@@ -19,15 +20,15 @@ CREATE TABLE Seance (
   CONSTRAINT Seance_fk_1 FOREIGN KEY (atelier) REFERENCES Atelier (id)
 );
 
--- CREATE TABLE Structure (
---   pk_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Clé primaire Structure',
---   Nom varchar(255) NOT NULL COMMENT 'Nom de la structure',
---   Telephone varchar(10) COMMENT 'Numéro de téléphone',
---   Mail varchar(255) COMMENT 'Adresse mail de la structure',
---   Description text COMMENT 'Informations complémentaire lié à la structure'
--- ) COMMENT='Table listant les structures';
--- 
---
+CREATE TABLE Structure (
+  id INTEGER PRIMARY KEY,
+  nom VARCHAR(255) NOT NULL,
+  telephone VARCHAR(10),
+  email VARCHAR(255),
+  description TEXT
+);
+
+
 -- CREATE TABLE Client (
 --   pk_id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Clé primaire',
 --   Nom varchar(16) NOT NULL COMMENT 'Nom de la personne',
